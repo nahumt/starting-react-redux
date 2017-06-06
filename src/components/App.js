6e2e8ab24f883e990864ae916630ce7a7f88a0eb
Nahum Trinidad <nahum@crehana.com>
2017-06-06 15:40:32 -0500

    create app layout

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class App extends Component {
+  render(){
+    return(
+      <div className="container-fluid">
+      {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+export default App;
