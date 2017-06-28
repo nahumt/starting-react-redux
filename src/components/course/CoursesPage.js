3aedc6fa503adc6dd7b17a47561486f75a95bd94
Nahum Trindad <nahum.trinidadv@gmail.com>
2017-06-28 00:17:06 -0500

    fixed binding

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,12 +1,38 @@
 import React, {Component, PropTypes} from 'react';
 
 class CoursesPage extends Component {
-  render(){
-    return(
-      <div>
-        <h1> Courses </h1>
-      </div>
-    );
-  }
+	constructor(props, context) {
+		super(props, context);
+		this.state = {
+			course: {title: ''}
+		};
+		this.onChangeTitle = this.onChangeTitle.bind(this);
+		this.onClickSave = this.onClickSave.bind(this);
+	}
+
+	onChangeTitle(e) {
+		const {course} = this.state;
+		course.title = e.target.value;
+		this.setState({course});
+	}
+	onClickSave() {
+		const {course} = this.state;
+		alert(`course ${course.title}`);
+	}
+
+	render() {
+		return (
+			<div>
+				<h1> Courses </h1>
+				<h2>Add Couress</h2>
+				<input
+					type="text"
+					value={this.state.course.title}
+					onChange={this.onChangeTitle}
+				/>
+				<input value="Save" type="submit" onClick={this.onClickSave} />
+			</div>
+		);
+	}
 }
 export default CoursesPage;
